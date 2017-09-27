45eebaee7adc5c15a86c1a34faba6f608acd44db
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-27 11:36:01 +0200

    compile directive

--- app/my-stars/my-stars.js
+++ app/my-stars/my-stars.js
@@ -1,11 +1,23 @@
 const app = angular.module('my-stars', []);
 
-app.directive('myStars', function() {
+app.directive('myStars', function () {
     return {
         controller: function MyStarsCtrl() {
             'ngInject';
             console.log('MyStarsCtrl', arguments);
-        }
+        },
+        compile: function () {
+            console.log('myStars compile', arguments);
+            return {
+                pre: function () {
+                    console.log('myStars prelink', arguments);
+                },
+                post: function () {
+                    console.log('myStars postlink', arguments);
+                }
+            }
+        },
+
     };
 });
 
