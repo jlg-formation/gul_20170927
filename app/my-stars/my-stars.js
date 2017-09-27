c4a9cd61d1c5828f49afdcb38c5b37183118775a
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-27 13:50:59 +0200

    stars

--- app/my-stars/my-stars.js
+++ app/my-stars/my-stars.js
@@ -4,29 +4,41 @@ app.directive('myStars', function () {
     return {
         restrict: 'E',
         scope: {
-            note: '='
+            note: '<'
         },
-//         template: `
-// <img ng-src="{{$ctrl.getImage(0)}}">
-// <img ng-src="{{$ctrl.getImage(1)}}">
-// <img ng-src="{{$ctrl.getImage(2)}}">
-// <img ng-src="{{$ctrl.getImage(3)}}">
-// <img ng-src="{{$ctrl.getImage(4)}}">
-//         `,
-        controller: function MyStarsCtrl($scope, $element, $attrs) {
+        //         template: `
+        // <img ng-src="{{$ctrl.getImage(0)}}">
+        // <img ng-src="{{$ctrl.getImage(1)}}">
+        // <img ng-src="{{$ctrl.getImage(2)}}">
+        // <img ng-src="{{$ctrl.getImage(3)}}">
+        // <img ng-src="{{$ctrl.getImage(4)}}">
+        //         `,
+        controller: function MyStarsCtrl($scope, $element, $attrs, $compile) {
             'ngInject';
             console.log('MyStarsCtrl', arguments);
             console.log('$scope.note', $scope.note);
 
-            let html= '';
-            let note = $scope.note || 0;
-            for (let i = 0; i < note; i++) {
-                html += '<img src="my-stars/img/yellow_star.png">';      
-            }
-            for (let i = note; i < 5; i++) {
-                html += '<img src="my-stars/img/white_star.png">';      
-            }
-            $element.html(html);
+            $scope.update = (newNote) => {
+                console.log('newNote', newNote);
+                $scope.note = newNote;
+            };
+
+            $scope.$watch('note', function () {
+                let html = '';
+                let note = $scope.note || 0;
+                note = (note > 5) ? 5 : note;
+                note = (note < 0) ? 0 : note;
+                for (let i = 0; i < note; i++) {
+                    html += '<img ng-click="update(' + (i + 1) + ')" src="my-stars/img/yellow_star.png">';
+                }
+                for (let i = note; i < 5; i++) {
+                    html += `<img ng-click="update(${i + 1})" src="my-stars/img/white_star.png">`;
+                }
+                $element.html(html);
+                $compile($element.contents())($scope);
+            });
+
+
         },
         controllerAs: '$ctrl',
         // compile: function (tElement, tAttrs, transclude) {
