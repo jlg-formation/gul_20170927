63cad0af862e8f3ccf674f16e77624710aa30da1
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-27 16:21:07 +0200

    add route

---
+++ app/my-layout/my-layout.config.js
@@ -0,0 +1,30 @@
+import homeHtml from './tmpl/home.html'
+import productsHtml from './tmpl/products.html'
+import servicesHtml from './tmpl/services.html'
+import contactHtml from './tmpl/contact.html'
+
+export const config = function ($stateProvider) {
+    'ngInject';
+
+    $stateProvider.state({
+        name: 'home',
+        url: '/',
+        template: homeHtml
+    });
+    $stateProvider.state({
+        name: 'products',
+        url: '/products',
+        template: productsHtml
+    });
+    $stateProvider.state({
+        name: 'services',
+        url: '/services',
+        template: servicesHtml
+    });
+    $stateProvider.state({
+        name: 'contact',
+        url: '/contact',
+        template: contactHtml
+    });
+}
+
--- app/my-layout/my-layout.js
+++ app/my-layout/my-layout.js
@@ -1,11 +1,15 @@
 import '../my-widgets/my-widgets.js';
 
+import 'angular-ui-router';
 
+
+import { config } from './my-layout.config.js';
 import { myHeader } from './my-header.component.js';
 import { myBody } from './my-body.component.js';
 import { myFooter } from './my-footer.component.js';
 
-angular.module('my-layout', ['my-widgets'])
+angular.module('my-layout', ['my-widgets', 'ui.router'])
+    .config(config)
     .component('myHeader', myHeader)
     .component('myBody', myBody)
     .component('myFooter', myFooter);
