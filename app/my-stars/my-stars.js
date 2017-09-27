59be1ca4d27ec993cb7dc3e07b79ea1b32a50489
Jean-Louis GUENEGO <jlguenego@gmail.com>
2017-09-27 14:06:56 +0200

    convert to component

--- app/my-stars/my-stars.js
+++ app/my-stars/my-stars.js
@@ -1,59 +1,40 @@
 const app = angular.module('my-stars', []);
 
-app.directive('myStars', function () {
-    return {
-        restrict: 'E',
-        scope: {
-            note: '<'
-        },
-        //         template: `
-        // <img ng-src="{{$ctrl.getImage(0)}}">
-        // <img ng-src="{{$ctrl.getImage(1)}}">
-        // <img ng-src="{{$ctrl.getImage(2)}}">
-        // <img ng-src="{{$ctrl.getImage(3)}}">
-        // <img ng-src="{{$ctrl.getImage(4)}}">
-        //         `,
-        controller: function MyStarsCtrl($scope, $element, $attrs, $compile) {
-            'ngInject';
-            console.log('MyStarsCtrl', arguments);
-            console.log('$scope.note', $scope.note);
+app.component('myStars', {
+    bindings: {
+        note: '=?'
+    },
+    controller: function MyStarsCtrl($scope, $element, $attrs, $compile) {
+        'ngInject';
+        console.log('MyStarsCtrl', arguments);
 
-            $scope.update = (newNote) => {
-                console.log('newNote', newNote);
-                $scope.note = newNote;
-            };
+        this.$onInit = function () {
+            console.log('ctrl.note', this.note);
 
-            $scope.$watch('note', function () {
-                let html = '';
-                let note = $scope.note || 0;
-                note = (note > 5) ? 5 : note;
-                note = (note < 0) ? 0 : note;
-                for (let i = 0; i < note; i++) {
-                    html += '<img ng-click="update(' + (i + 1) + ')" src="my-stars/img/yellow_star.png">';
-                }
-                for (let i = note; i < 5; i++) {
-                    html += `<img ng-click="update(${i + 1})" src="my-stars/img/white_star.png">`;
-                }
-                $element.html(html);
-                $compile($element.contents())($scope);
-            });
+        }
+
+        $scope.update = (newNote) => {
+            console.log('newNote', newNote);
+            this.note = newNote;
+        };
+
+        $scope.$watch('$ctrl.note', () => {
+            let html = '';
+            let note = this.note || 0;
+            note = (note > 5) ? 5 : note;
+            note = (note < 0) ? 0 : note;
+            for (let i = 0; i < note; i++) {
+                html += '<img ng-click="update(' + (i + 1) + ')" src="my-stars/img/yellow_star.png">';
+            }
+            for (let i = note; i < 5; i++) {
+                html += `<img ng-click="update(${i + 1})" src="my-stars/img/white_star.png">`;
+            }
+            $element.html(html);
+            $compile($element.contents())($scope);
+        });
 
 
-        },
-        controllerAs: '$ctrl',
-        // compile: function (tElement, tAttrs, transclude) {
-        //     console.log('myStars compile', arguments);
-        //     return {
-        //         pre: function () {
-        //             console.log('myStars prelink', arguments);
-        //         },
-        //         post: function () {
-        //             console.log('myStars postlink', arguments);
-        //         }
-        //     }
-        // },
-
-    };
+    }
 });
 
 
